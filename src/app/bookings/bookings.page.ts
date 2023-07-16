cca11de246b93392f0d1db17c8a6a95ceb982a21
Renzo Hernandez <112911770+hernandezrenzo@users.noreply.github.com>
2023-07-16 20:50:14 +0800

    updates

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -11,6 +11,29 @@ export class BookingsPage implements OnInit {
 
   ngOnInit() {
   }
+  highlightedDates = [
+    {
+      date: '2023-07-05',
+      textColor: '#fff',
+      backgroundColor: '#E3242B',
+    },
+    {
+      date: '2023-07-10',
+      textColor: '#fff',
+      backgroundColor: '#E3242B',
+    },
+    {
+      date: '2023-07-20',
+      textColor: '#fff',
+      backgroundColor: '#E3242B',
+    },
+    {
+      date: '2023-07-23',
+      textColor: '#fff',
+      backgroundColor: '#E3242B',
+    },
+  ];
+  
   public alertButtons = ['OK'];
 
 }
